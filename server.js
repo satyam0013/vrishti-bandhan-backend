8dd14ec514e0c21b820c6bc4d2451a8cfefa242a
satyam0013 <satyammule1303@gmail.com>
2025-04-11 16:39:55 +0530

    Fix bcrypt issue by replacing with bcryptjs and reinstalling dependencies

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs');
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 
